a1a55fa747d584a5ca9363d38c618050d317de80
celdond <celdond@gmail.com>
2023-11-01 23:25:51 -0700

    added basic database handling

--- data/database_access.gd
+++ data/database_access.gd
@@ -1,1 +1,17 @@
 extends Node
+
+var db : SQLite = SQLite.new()
+
+const verbosity_level : int = SQLite.VERBOSE
+
+var db_name := "res://data/database.db"
+
+func _ready():
+	db.path = db_name
+	db.verbosity_level = verbosity_level
+
+func get_compliments():
+	db.open_db()
+	var selected = db.select_rows("standard", "", ["*"])
+	db.close_db()
+	return selected
