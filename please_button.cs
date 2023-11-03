fad48a41499dee27e759b65a3456da479a7c7311
celdond <celdond@gmail.com>
2023-11-02 23:14:12 -0700

    work to get text display

---
+++ please_button.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class GameLoop : Node2D 
+
+{
+	Label compliment;
+	public override void _Ready()
+	{
+		compliment = (Label)GetNode("Control/Margin/CanvasLayer/Compliment");
+		compliment.Text = "Press the button to receive a compliment.";
+	}
+	
+	private void _on_button_down()
+	{
+		compliment.Text = "Random!";	
+	}
+}
