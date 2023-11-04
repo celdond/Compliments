e48f8421c7c393136452c678d7127535a681d382
celdond <celdond@gmail.com>
2023-11-04 00:35:13 -0700

    attached database access to gameloop

--- GameLoop.cs
+++ GameLoop.cs
@@ -1,13 +1,17 @@
 using Godot;
 using System;
-using 
 
 public partial class GameLoop : Node2D 
 
 {
 	Label compliment;
+	GodotObject database;
+	Variant compliments;
 	public override void _Ready()
 	{
+		GDScript DatabaseScript = (GDScript)GD.Load("res://data/database_access.gd");
+		database = (GodotObject)DatabaseScript.New();
+		compliments = (Variant)database.Call("get_compliments");
 		compliment = (Label)GetNode("./Control/Margin/CanvasLayer/Compliment");
 		compliment.Text = "Press the button to receive a compliment.";
 	}
--- data/database_access.gd
+++ data/database_access.gd
@@ -4,13 +4,11 @@ var db : SQLite = SQLite.new()
 
 const verbosity_level : int = SQLite.VERBOSE
 
-var db_name := "res://data/content/compliments_database.db"
-
-func _ready():
-	db.path = db_name
-	db.verbosity_level = verbosity_level
+var db_name := "res://data/content/compliments_database"
 
 func get_compliments():
+	db.path = db_name
+	db.verbosity_level = verbosity_level
 	db.open_db()
 	var selected = db.select_rows("standard", "", ["*"])
 	db.close_db()
