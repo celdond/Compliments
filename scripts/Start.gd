4df42c4e8abb491011ac92ab5f04d6884b8962ee
celdond <celdond@gmail.com>
2025-01-16 00:09:27 -0800

    button click logic for stat screen

--- scripts/Start.gd
+++ scripts/Start.gd
@@ -1,5 +1,13 @@
 extends TextureButton
 
 func _on_pressed():
+	$".".disabled = true
 	var game_scene: String = "res://scenes/compliment.tscn"
+	$ClickDown.play()
+	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file(game_scene)
+	return
+
+#Ignore / Overwrite
+func _on_button_down() -> void:
+	pass
