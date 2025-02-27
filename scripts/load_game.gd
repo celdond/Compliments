6a8cdb2a2683c2bbf7f8c2411e33c737da3d6385
celdond <celdond@gmail.com>
2025-02-27 00:28:01 -0800

    Comments and Clean-up on Start-up screen complete

--- scripts/load_game.gd
+++ scripts/load_game.gd
@@ -1,23 +1,32 @@
 extends Node2D
+
+# Load Libraries
+var util: Object
+const util_script = preload("res://scripts/util.gd")
+
+# Load Configuration File
 var config = ConfigFile.new()
 var err = config.load("user://settings.cfg")
-const util_script = preload("res://scripts/util.gd")
-var util: Object
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	util = util_script.new()
+	
+	# If there is an error, generate a new config file with default settings
 	if err != OK:
 		config.set_value("Display", "Mode", 1)
 		config.set_value("Display", "Resolution", 1)
 		config.set_value("Audio", "Volume", 0.0)
 		config.save("user://settings.cfg")
 		return
+	
+	# Else retrieve saved configurations
 	else:
 		var displayMode = config.get_value("Display", "Mode")
 		var resolution = config.get_value("Display", "Resolution")
 		var volume = config.get_value("Audio", "Volume")
-		print(resolution)
+		
+		# Apply the settings from the saved configuration file
 		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), volume)
 		util.screen_size_change(resolution)
 		util.mode_change(displayMode)
